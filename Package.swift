dee696d0b67656948f25a1ef807c70674049f0a6
ramen <48982211+KikuchiTomo@users.noreply.github.com>
2024-08-24 22:09:54 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
   products: [
     .library(
       name: "ReactiveObjCBridge",
-      targets: ["ReactiveObjCBridgeTarget"])     
+      targets: ["ReactiveObjCBridge"])     
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.4.0"),
@@ -15,16 +15,14 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(
-      name: "ReactiveObjCBridge",
+      name: "ReactiveObjCBridgeTarget",
       url: "https://github.com/KikuchiTomo/XCReactiveObjCBridge/releases/download/v1.0.0/ReactiveObjCBridge.xcframework.zip",
       checksum: "57a0b762b357cb222c95993d1d5be70678e3696daa9cb176b6675a59d9a25306"
      ),
-     .target(name: "ReactiveObjCBridgeTarget",
+     .target(name: "ReactiveObjCBridge",
               dependencies: [
-                  .target(name: "ReactiveObjCBridge"),            
+                  .target(name: "ReactiveObjCBridgeTarget"),            
                   .product(name: "ReactiveSwift", package: "ReactiveSwift")
-              ],
-              path: "XCReactiveObjCBridgeTarget"
-        )
+              ])
   ]
 )
