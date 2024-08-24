27a8cb84c5bf1f8df0f8da9c979abc43db23146f
ramen <48982211+KikuchiTomo@users.noreply.github.com>
2024-08-24 19:37:32 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   targets: [
      .binaryTarget(
       name: "ReactiveObjCBridge",
-      url: "https://github.com/KikuchiTomo/XCReactiveObjC/releases/download/v1.0.0/ReactiveObjCBridge.xcframework.zip",
+      url: "https://github.com/KikuchiTomo/XCReactiveObjCBridge/releases/download/v1.0.0/ReactiveObjCBridge.xcframework.zip",
       checksum: "57a0b762b357cb222c95993d1d5be70678e3696daa9cb176b6675a59d9a25306"
      )       
   ]
